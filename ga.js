d87c7bec47cd964f6ca1c00165dfa66bedc9ab71
mohammedfarish <farishmusthafa@gmail.com>
2022-10-23 02:01:50 +0400

    Updates

---
+++ ga.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-inner-declarations */
+/* eslint-disable prefer-rest-params */
+/* eslint-disable no-undef */
+if (navigator.userAgent !== "ReactSnap") {
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+  gtag("js", new Date());
+  gtag("config", "G-RLZ49MGWPV");
+}
